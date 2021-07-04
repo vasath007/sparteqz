582ea61bb7f277e3735aeb52ad519bf15219a9c2
rajasekar <rajasekar@xvaluetech.com>
2021-07-04 15:54:45 +0530

    one space added

--- lib/reusuable_widgets/food_items.dart
+++ lib/reusuable_widgets/food_items.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sparteqz/model/food_model.dart';
 
+
 class FoodItem extends StatefulWidget
 {
   List<FoodDetails> foodDetails;
